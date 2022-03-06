791bb9e06cf278a0bc0fb95a560a6e779bda907f
박힘찬 <himchan05@naver.com>
2022-03-06 20:31:04 +0900

    Initial Commit

---
+++ MyText/MyTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTextApp.swift
+//  MyText
+//
+//  Created by 박힘찬 on 2022/03/06.
+//
+
+import SwiftUI
+
+@main
+struct MyTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
